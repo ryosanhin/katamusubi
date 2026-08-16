c44a2826b48a7d1ccf2cf563a10eaf3590763864
ryosanhin <90621212+ryosanhin@users.noreply.github.com>
2026-08-16 15:09:05 +0900

    change: クラス名の変更とそれに伴うAPIの変更

--- addons/tscn_scanner/runtime/scope_definition.gd
+++ addons/tscn_scanner/runtime/scope_definition.gd
@@ -17,23 +17,23 @@ class_name ScopeDefinition
 ## 親スコープのID
 @export var parent_scope_id: StringName
 
-## スコープのプロパティを作成[br]
+## スコープ定義を作成[br]
 ## [param init_scene_uid]: スコープのノードが存在するシーンのuid[br]
 ## [param init_node_path]: スコープのノードのツリーでの場所[br]
 ## [param init_scope_name]: スコープのノードの名前[br]
 ## [param init_scope_id]: スコープのID[br]
 ## [param init_parent_scope_id]: 親スコープのID
-static func create_new_property(
+static func create_new_definition(
 	init_scene_uid: String,
 	init_node_path: NodePath,
 	init_scope_name: StringName,
 	init_scope_id: StringName,
 	init_parent_scope_id: StringName,
 ) -> ScopeDefinition:
-	var property := ScopeDefinition.new()
-	property.scene_uid = init_scene_uid
-	property.node_path = init_node_path
-	property.scope_name = init_scope_name
-	property.scope_id = init_scope_id
-	property.parent_scope_id = init_parent_scope_id
-	return property
+	var definition := ScopeDefinition.new()
+	definition.scene_uid = init_scene_uid
+	definition.node_path = init_node_path
+	definition.scope_name = init_scope_name
+	definition.scope_id = init_scope_id
+	definition.parent_scope_id = init_parent_scope_id
+	return definition
