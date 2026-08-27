d45dad3b12a648ea2b9e4eced8e6556e47e72874
Ryosanhin <90621212+ryosanhin@users.noreply.github.com>
2026-08-27 16:10:22 +0900

    fix: 未保存シーンに存在するスコープIDを考慮したユニークID生成を実装 (#30)

---
+++ addons/katamusubi/editor/scope_id_generator.gd
@@ -0,0 +1,52 @@
+@tool
+extends RefCounted
+
+const RandomID := preload("random_id.gd")
+
+## スコープ用グループ名
+const CONTAINER_GROUP := &"test_group"
+
+
+## 新規スコープIDを取得。[br]
+## 100回生成して新規IDが生成できなかった場合は[code]&""[/code]を返す。[br]
+## [param existed_ids]: 既存スコープID
+static func get_unique_id(existed_ids: Array[StringName]) -> StringName:
+	var tmp_ids: Array[StringName] = []
+	# エディタで開いているシーンを全取得
+	var opened_roots := EditorInterface.get_open_scene_roots()
+	for root in opened_roots:
+		if root == null:
+			continue
+		tmp_ids.append_array(_get_scope_ids_in_subtree(root))
+	existed_ids.append_array(tmp_ids)
+
+	var id := RandomID.get_random_id()
+	var loop_count := 1
+	const MAX_LOOP_COUNT := 100
+
+	while id in existed_ids:
+		if loop_count >= MAX_LOOP_COUNT:
+			push_error("uid生成ループ回数が上限に達しました。")
+			return &""
+		id = RandomID.get_random_id()
+		loop_count += 1
+
+	return id
+
+
+## 子ノード内に存在するスコープを検索し存在したスコープIDを返す。[br]
+## [param root]: 検索する始点のノード
+static func _get_scope_ids_in_subtree(root: Node) -> Array[StringName]:
+	var scope_ids: Array[StringName] = []
+	var nodes: Array[Node] = [root]
+	
+	while not nodes.is_empty():
+		var node := nodes.pop_back()
+
+		var scope := node as ContainerScope
+		if scope != null:
+			scope_ids.append(scope.scope_id)
+
+		nodes.append_array(node.get_children())
+	
+	return scope_ids
