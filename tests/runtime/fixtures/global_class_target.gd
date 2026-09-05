0fb4cfa6bd2515bad8f7ea4520ba569302a59c36
ryosanhin <90621212+ryosanhin@users.noreply.github.com>
2026-09-05 22:57:14 +0900

    test: テストを削除（テストは他PRで追加予定)

--- tests/runtime/fixtures/global_class_target.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-var injected_service: InjectionTestGlobalService
-
-
-func inject_dependency(service: InjectionTestGlobalService) -> void:
-	injected_service = service
--- tests/runtime/fixtures/unresolved_class_target.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-const AnonymousService := preload("anonymous_service.gd")
-
-
-func inject_dependency(_service: AnonymousService) -> void:
-	pass
