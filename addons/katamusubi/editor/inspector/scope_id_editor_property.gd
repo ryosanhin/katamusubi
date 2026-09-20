8db1b079ab72fa9d67d43a82ffcc2db401e7d1a0
ryosanhin <90621212+ryosanhin@users.noreply.github.com>
2026-09-20 14:30:24 +0900

    feat!: プルダウン式から候補列挙式に変更

---
+++ addons/katamusubi/editor/inspector/scope_id_editor_property.gd
@@ -0,0 +1,32 @@
+@tool
+extends EditorProperty
+
+var _line_edit: LineEdit
+
+
+func _init() -> void:
+	_line_edit = LineEdit.new()
+	_line_edit.placeholder_text = "empty = private scope"
+	_line_edit.text_submitted.connect(_on_text_submitted)
+	_line_edit.focus_exited.connect(_on_focus_exited)
+	add_child(_line_edit)
+
+
+func _update_property() -> void:
+	var value := get_edited_object().get(get_edited_property()) as StringName
+	_line_edit.text = value
+
+
+func _on_text_submitted(text: String) -> void:
+	_commit(StringName(text))
+
+
+func _on_focus_exited() -> void:
+	_commit(StringName(_line_edit.text))
+
+
+func _commit(value: StringName) -> void:
+	if get_edited_object().get(get_edited_property()) == value:
+		return
+
+	emit_changed(get_edited_property(), value)
