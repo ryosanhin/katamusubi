b1444e02b1c10cc2997a11bcac2f9f363521acb4
Ryosanhin <90621212+ryosanhin@users.noreply.github.com>
2026-09-21 23:37:52 +0900

    delete: スキャン時にインスタンスされたシーンを除外する処理を削除 (#108)

--- addons/katamusubi/editor/scanning/tscn_scanner.gd
+++ addons/katamusubi/editor/scanning/tscn_scanner.gd
@@ -11,9 +11,6 @@ static func scan(scene_uid: StringName) -> SceneSnapshot:
 	var entries: Array[ScopeSnapshot] = []
 	var state := packed_scene.get_state()
 	for node_index in state.get_node_count():
-		if state.get_node_instance(node_index) != null:
-			continue
-		
 		var script: Script
 		var scope_id := &""
 
