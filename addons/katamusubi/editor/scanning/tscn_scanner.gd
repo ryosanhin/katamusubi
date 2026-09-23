4bc9650967cfef3abf5762e19d844fc386721f59
Ryosanhin <90621212+ryosanhin@users.noreply.github.com>
2026-09-23 16:03:14 +0900

    fix: coderabbitai に指摘された箇所の修正

--- addons/katamusubi/editor/scanning/tscn_scanner.gd
+++ addons/katamusubi/editor/scanning/tscn_scanner.gd
@@ -8,7 +8,7 @@ static func scan(scene_uid: StringName) -> SceneSnapshot:
 	var packed_scene := ResourceLoader.load(
 			scene_uid,
 			"PackedScene",
-			ResourceLoader.CACHE_MODE_IGNORE
+			ResourceLoader.CACHE_MODE_REPLACE_DEEP
 	) as PackedScene
 
 	if packed_scene == null:
